f769368e9f9c4f02edb2472e1e466c432d439629
kxtmg <disizkatie@gmail.com>
2024-09-30 15:01:49 -0700

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "./components/SideBar";
+
+function App() {
+  return (
+    <>
+      <Sidebar/>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,8 @@
+export default function Sidebar() {
+    return (
+        <>
+            <h1> Your Projects </h1>
+            <button> + Add Project </button>
+        </>
+    );
+}
